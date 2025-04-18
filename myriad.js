fada8173ed9aaf9a3c27cb7178bd740ab160614f
Myriad <taylor135@gresham.k12.or.us>
2025-04-18 11:07:45 -0700

    Delete myriad.js

--- myriad.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('box').addEventListener('change', function() {
-	    var icon = document.getElementById('icon');
-	    if (this.checked) {
-		icon.setAttribute('target', '_blank');
-	    } else {
-		icon.removeAttribute('target');
-	    }
-	});
